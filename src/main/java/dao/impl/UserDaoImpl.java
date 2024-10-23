114ee4ab09095b026cac68988e43c73b95850728
Parviz Namazli <55592138+Perviz021@users.noreply.github.com>
2024-10-23 14:14:28 +0400

    added new user properties

--- src/main/java/dao/impl/UserDaoImpl.java
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package dao.impl;
 
-import bean.User;
+import entity.Nationality;
+import entity.User;
 import dao.inter.AbstractDao;
 import dao.inter.UserDaoInter;
 
@@ -9,25 +10,43 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class UserDaoImpl extends AbstractDao implements UserDaoInter {
+    private User getUser(ResultSet rs) throws Exception {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String surname = rs.getString("surname");
+        String email = rs.getString("email");
+        String phone = rs.getString("phone");
+        int birthPlaceId = rs.getInt("birthplace_id");
+        String birthPlaceStr = rs.getString("birthplace");
+        int nationalityId = rs.getInt("nationality_id");
+        String nationalityStr = rs.getString("nationality");
+        Date birthDate = rs.getDate("birthDate");
+
+        Nationality nationality = new Nationality(nationalityId, nationalityStr, null);
+        Nationality birthPlace = new Nationality(birthPlaceId, birthPlaceStr, null);
+
+        return new User(id, name, surname, email, phone, birthDate, nationality, birthPlace);
+    }
+
     @Override
     public List<User> getAll() {
         List<User> result = new ArrayList<>();
         try {
             Connection connection = connect();
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("Select * from user");
+            ResultSet rs = stmt.executeQuery("select " +
+                    "u.*, n.name as nationality, c.country_name as birthplace " +
+                    "from user u " +
+                    "left join nationality n on u.nationality_id = n.id " +
+                    "left join nationality c on u.birthplace_id = c.id;");
 
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                String email = rs.getString("email");
-                String phone = rs.getString("phone");
-
-                result.add(new User(id, name, surname, email, phone));
+                User u = getUser(rs);
+                result.add(u);
             }
             connection.close();
         } catch (Exception ex) {
@@ -43,16 +62,14 @@ public class UserDaoImpl extends AbstractDao implements UserDaoInter {
         try {
             Connection connection = connect();
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("Select * from user where id=" + userId);
+            ResultSet rs = stmt.executeQuery("select " +
+                    "u.*, n.name as nationality, c.country_name as birthplace " +
+                    "from user u " +
+                    "left join nationality n on u.nationality_id = n.id " +
+                    "left join nationality c on u.birthplace_id = c.id where id=" + userId);
 
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                String email = rs.getString("email");
-                String phone = rs.getString("phone");
-
-                result = new User(id, name, surname, email, phone);
+                result = getUser(rs);
             }
             connection.close();
         } catch (Exception ex) {
@@ -106,4 +123,5 @@ public class UserDaoImpl extends AbstractDao implements UserDaoInter {
             return false;
         }
     }
+
 }
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,120 @@
+package entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private Date birthDate;
+    private Nationality nationality;
+    private Nationality birthPlace;
+    private List<UserSkill> skills;
+
+    public User() {
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public User(int id, String name, String surname, String email, String phone, Date birthDate, Nationality nationality, Nationality birthPlace) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+        this.birthPlace = birthPlace;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Nationality getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(Nationality nationality) {
+        this.nationality = nationality;
+    }
+
+    public Nationality getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(Nationality birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public List<UserSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<UserSkill> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthDate=" + birthDate +
+                ", nationality=" + nationality +
+                ", birthPlace=" + birthPlace +
+                '}';
+    }
+}
